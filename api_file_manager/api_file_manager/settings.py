0a692b1b9ee04bddae772fd21d5788386d247539
keep-you-busy <twiddle_thumbs@aol.com>
2023-10-26 14:28:10 +0300

    Env naming fix

--- api_file_manager/api_file_manager/settings.py
+++ api_file_manager/api_file_manager/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
 
 # Configure Celery settings
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 
 
 # Password validation
