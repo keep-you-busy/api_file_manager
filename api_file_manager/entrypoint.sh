f0df8f7e1f03fa406153561e176db1353d1118a8
keep-you-busy <twiddle_thumbs@aol.com>
2023-10-26 22:39:44 +0300

    gunicorn entrypoint fix

--- api_file_manager/entrypoint.sh
+++ api_file_manager/entrypoint.sh
@@ -4,7 +4,7 @@ cd /usr/src/app/api_file_manager
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn api_file_manager.wsgi:application --bind 0.0.0.0:8000 --workers 4
+gunicorn api_file_manager.wsgi
 
 celery -A api_file_manager worker --loglevel=info --logfile=logs/celery.log &
 
