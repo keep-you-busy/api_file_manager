b14b371ef3f9aa3401032535227d19a5ab529d5e
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-10-27 01:41:01 +0300

    Configuration for docker compose

--- api_file_manager/entrypoint.sh
+++ api_file_manager/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-cd /usr/src/app/api_file_manager
 python manage.py makemigrations
 python manage.py migrate
-
-gunicorn api_file_manager.wsgi
-
-celery -A api_file_manager worker --loglevel=info --logfile=logs/celery.log &
-
-tail -f /dev/null
