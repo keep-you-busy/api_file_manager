4968dcffb28a8fc1ba86a0b2818e0b63ab89cbce
keep-you-busy <twiddle_thumbs@aol.com>
2023-10-26 14:10:50 +0300

    Celery tasks

---
+++ api_file_manager/api/tasks.py
@@ -0,0 +1,24 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+from api.models import File
+
+
+logger = get_task_logger(__name__)
+
+
+@shared_task
+def process_file(file_id):
+    try:
+        file = File.objects.get(pk=file_id)
+        logger.info(f"Processing file {file_id}")
+        with file.file.open('r') as file_content:
+            content = file_content.read()
+        if content:
+            file.content = content
+        file.processed = True
+        file.save()
+    except File.DoesNotExist:
+        logger.error(f"File {file_id} does not exist.")
+    except Exception as error:
+        logger.error(f"An error occurred with file {file_id}: {error}")
