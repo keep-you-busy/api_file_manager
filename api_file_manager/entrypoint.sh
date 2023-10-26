01ea6b41d4f188121a81aebe4eade26769b3c58a
keep-you-busy <twiddle_thumbs@aol.com>
2023-10-26 14:29:48 +0300

    Docker web container run

---
+++ api_file_manager/entrypoint.sh
@@ -0,0 +1,8 @@
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000 &
+
+celery -A api_file_manager worker --loglevel=info --logfile=logs/celery.log &
+
+tail -f /dev/null
