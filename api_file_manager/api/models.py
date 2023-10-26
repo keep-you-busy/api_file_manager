7cbc047ebc2a2a6401e5d431a14ee342ed89e3f7
keep-you-busy <twiddle_thumbs@aol.com>
2023-10-26 14:03:57 +0300

    File model

--- api_file_manager/api/models.py
+++ api_file_manager/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField(
+        verbose_name='Загруженный файл',
+        upload_to='uploads/')
+    uploaded_at = models.DateTimeField(
+        verbose_name='Дата загрузки',
+        auto_now_add=True,
+        editable=False)
+    processed = models.BooleanField(
+        verbose_name='Статус обработки',
+        default=False)
+    content = models.TextField(
+        verbose_name='Содержимое файла',
+        default=''
+    )
+
+    def __str__(self):
+        return f'{self.file.name}: {self.uploaded_at.date()}'
